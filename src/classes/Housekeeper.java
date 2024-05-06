48f32d03298ef7cb49e37d4666d0081069e31208
João Pedro Alcântara P. Calzá <112181440+Jotinha29@users.noreply.github.com>
2024-05-06 14:16:27 -0300

    Update Housekeeper.java
    Terminei a Classe.

--- src/classes/Housekeeper.java
+++ src/classes/Housekeeper.java
@@ -1,4 +1,28 @@
 package classes;
+import java.util.List;
 
-public class Housekeeper {
+public class Housekeeper implements Runnable {
+    private final List<Room> rooms;
+
+    public Housekeeper(int id, List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(15000);
+                synchronized (rooms) {
+                    for (Room room : rooms) {
+                        if (room.isOccupied() && room.isClean() && room.isKeyAtFrontDesk()) {
+                            room.clean();
+                        }
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.fillInStackTrace();
+        }
+    }
 }
